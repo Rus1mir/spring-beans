e4271ef3afa5362005e22512506d71ce2cab4d42
Rus1mir <rusmir@ukr.net>
2021-05-06 22:00:28 +0300

    Create Dog bean and test automatic DI with beans throw his names and qualifiers

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,41 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    @Autowired
+    @Qualifier("cat")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+
 }
